bb32557f4f78f11c88b719f46cba7648bc18e671
PaulaJansen <paulamarijkejansen@gmail.com>
2025-05-25 13:58:56 +0200

    Methodes en variabelen aangemaakt

--- src/homework.java
+++ src/homework.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class homework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        int input1 = 0;
+        int input2 = 0;
+        String name = new String();
+
+
+
+    }
+
+    public static void hello() {
+        System.out.println("Hello, world!");
+    }
+
+    public static int postiveOrNegative() {
+        Scanner scanner = new Scanner(System.in);
+        return;
+    }
+
+    public static int positiveOrZeroOrNegative() {
+        Scanner scanner = new Scanner(System.in);
+        return;
+    }
+
+    public static String bartender(){
+        Scanner scanner = new Scanner(System.in);
+        return;
+    }
+
+    public static void sum(int input1, int input2){
+
+    }
+
 }
+
