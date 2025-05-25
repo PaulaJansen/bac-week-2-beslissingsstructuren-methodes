e784c2e9598dcfcaf04e2b0844c81cb34c598468
PaulaJansen <paulamarijkejansen@gmail.com>
2025-05-25 14:44:11 +0200

    Opdracht volledig uitgewerkt

--- src/homework.java
+++ src/homework.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class homework {
     public static void main(String[] args) {
+
+        hello();
+
         Scanner scanner = new Scanner(System.in);
-        int number = 0;
-        int input1 = 0;
-        int input2 = 0;
-        String name = new String();
 
+        System.out.println("Voer een nummer in: ");
+        int number = scanner.nextInt();
+        postiveOrNegative(number);
 
+        System.out.println("Voer een nummer in: ");
+        number = scanner.nextInt();
+        positiveOrZeroOrNegative(number);
+
+        System.out.println("Wat is je favoriete cocktail?");
+        String name = scanner.next();
+        bartender(name);
+
+        System.out.println("Voer een nummer in:");
+        int input1 = scanner.nextInt();
+        System.out.println("Voer nog een nummer in:");
+        int input2 = scanner.nextInt();
+        int result = sum(input1, input2);
+        System.out.println("De som is " + result + ".");
 
     }
 
@@ -16,23 +32,54 @@ public class homework {
         System.out.println("Hello, world!");
     }
 
-    public static int postiveOrNegative() {
-        Scanner scanner = new Scanner(System.in);
-        return;
+    public static void postiveOrNegative(int number) {
+        if (number >= 0){
+            System.out.println("This number is positive!");
+        } else {
+            System.out.println("This number is negative!");
+        }
     }
 
-    public static int positiveOrZeroOrNegative() {
-        Scanner scanner = new Scanner(System.in);
-        return;
+    public static void positiveOrZeroOrNegative(int number) {
+        if (number > 0){
+            System.out.println("This number is positive!");
+        } else if (number < 0){
+            System.out.println("This number is negative!");
+        } else {
+            System.out.println("This number is zero!");
+        }
     }
 
-    public static String bartender(){
-        Scanner scanner = new Scanner(System.in);
-        return;
+    public static void bartender(String name){
+        switch (name) {
+            case "Magarita":
+            case "magarita":
+                System.out.println("Mijn favoriete drankje is een magarita.");
+                break;
+            case "Gin tonic":
+            case "gin tonic":
+            case "GT":
+            case "gt":
+                System.out.println("Mijn favoriete drankje is een gin tonic.");
+                break;
+            case "Espresso martini":
+            case "espresso martini":
+            case "Esma":
+            case "esma":
+                System.out.println("Mijn favoriete drankje is een espresso martini");
+                break;
+            case "Daiquiri":
+            case "daiquiri":
+                System.out.println("Mijn favoriete drankje is een daiquiri.");
+                break;
+            default:
+                System.out.println("Joh, ik drink alles!");
+        }
+
     }
 
-    public static void sum(int input1, int input2){
-
+    public static int sum(int input1, int input2){
+        return input1 + input2;
     }
 
 }
